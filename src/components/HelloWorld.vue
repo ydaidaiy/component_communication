156ab4235772a8f3f286136ff9b3d8e80d85b5e1
ydaidaiy <329765409@qq.com>
2020-06-18 09:35:23 +0800

    跟新

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: dull_
  * @Date: 2020-05-07 14:38:48
- * @LastEditTime: 2020-05-22 15:15:13
+ * @LastEditTime: 2020-06-18 09:35:08
  * @LastEditors: dull_
  * @Description: 这是一个插槽组件
  * @FilePath: \component_communication\src\components\HelloWorld.vue
@@ -9,7 +9,7 @@
 -->
 <template>
   <div>
-    <div>你好哇</div>
+    <div>你好哇，你就是一个小孩子</div>
   </div>
 </template>
 
